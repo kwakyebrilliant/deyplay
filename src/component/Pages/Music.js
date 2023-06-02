53bacff82b6d005780ee4204246da3503755cf2c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-02 15:38:21 +0000

    music updates

--- src/component/Pages/Music.js
+++ src/component/Pages/Music.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import Sidebar from '../Partials/Sidebar'
+import PartialNavbar from '../Partials/PartialNavbar'
 
 function Music() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+            <div className='text-black'>
+            <>
+    
+            <div className='flex flex-auto'>
+                <Sidebar />
+    
+                <div className='grow'>
+                    <PartialNavbar />
+    
+                </div>
+    
+            </div>
+            </>
+            </div>
+        </div>
+      )
 }
 
 export default Music
