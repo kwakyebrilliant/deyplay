1cc0d1da544a56ac2800025fe979ce7e14b3893b
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-02 15:23:25 +0000

    home updates

--- src/component/Pages/Homepage.js
+++ src/component/Pages/Homepage.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Sidebar from '../Partials/Sidebar'
+import PartialNavbar from '../Partials/PartialNavbar'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+        <div className='text-black'>
+        <>
+
+        <div className='flex flex-auto'>
+            <Sidebar />
+
+            <div className='grow'>
+                <PartialNavbar />
+
+            </div>
+
+        </div>
+        </>
+        </div>
+    </div>
   )
 }
 
