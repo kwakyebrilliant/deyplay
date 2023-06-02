c8c7490d1fca75f67c1b4725bb98a982a2446bb2
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-02 20:33:58 +0000

    library added

---
+++ src/component/Pages/Library.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Sidebar from '../Partials/Sidebar'
+import PartialNavbar from '../Partials/PartialNavbar'
+
+function Library() {
+    return (
+        <div>
+            <div className='bg-black/80'>
+            <>
+    
+            <div className='flex flex-auto'>
+                <Sidebar />
+    
+                <div className='grow'>
+                    <PartialNavbar />
+    
+                </div>
+    
+            </div>
+            </>
+            </div>
+        </div>
+      )
+}
+
+export default Library
