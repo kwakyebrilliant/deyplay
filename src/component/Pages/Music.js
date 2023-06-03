28f9e0f63d9625c80ee01f1be8a21cdda4f789c0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-03 19:23:57 +0000

    music updates

--- src/component/Pages/Music.js
+++ src/component/Pages/Music.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Sidebar from '../Partials/Sidebar'
 import PartialNavbar from '../Partials/PartialNavbar'
+import coverimg from '../assets/coverimg.jpeg';
+import { FiSearch } from 'react-icons/fi';
 
 function Music() {
     return (
@@ -13,6 +15,22 @@ function Music() {
     
                 <div className='grow'>
                     <PartialNavbar />
+
+                    
+                    <div className="relative mx-2">
+                        <img src={coverimg} alt="Background" className="w-full rounded object-cover h-96" />
+                        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-pink-500 p-2">
+                            <input
+                            className="w-64 bg-transparent focus:outline-none text-white placeholder-white"
+                            type="text"
+                            placeholder="Search..."
+                            />
+                            <button className="ml-2 text-white">
+                            <FiSearch />
+                            </button>
+                        </div>
+                   
+                    </div>
     
                 </div>
     
