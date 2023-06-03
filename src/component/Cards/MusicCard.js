de4944780f94d4a817d7ac79fc3cd37dd486c667
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-03 11:38:43 +0000

    assets updates

--- src/component/Cards/MusicCard.js
+++ src/component/Cards/MusicCard.js
@@ -139,6 +139,138 @@ function MusicCard() {
         </div>
 
 
+        <div className='relative py-8 grid grid-cols-2 lg:grid-cols-6 gap-x-8 gap-y-16'>
+
+
+<article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+        <a href='#'>
+    <img
+        alt="Office"
+        src={coverimg}
+        className="w-full h-40 p-4 object-cover"
+    />
+
+    <div className="p-4 sm:p-6">
+        <h3 className="font-medium text-white">
+            John Doe
+        </h3>
+
+        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+        I can fly
+        </p>
+
+    </div>
+    </a>
+    </article>
+
+    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+        <a href='#'>
+    <img
+        alt="Office"
+        src={coverimg}
+        className="w-full h-40 p-4 object-cover"
+    />
+
+    <div className="p-4 sm:p-6">
+        <h3 className="font-medium text-white">
+            John Doe
+        </h3>
+
+        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+        I can fly
+        </p>
+
+    </div>
+    </a>
+    </article>
+
+    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+        <a href='#'>
+    <img
+        alt="Office"
+        src={coverimg}
+        className="w-full h-40 p-4 object-cover"
+    />
+
+    <div className="p-4 sm:p-6">
+        <h3 className="font-medium text-white">
+            John Doe
+        </h3>
+
+        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+        I can fly
+        </p>
+
+    </div>
+    </a>
+    </article>
+
+    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+        <a href='#'>
+    <img
+        alt="Office"
+        src={coverimg}
+        className="w-full h-40 p-4 object-cover"
+    />
+
+    <div className="p-4 sm:p-6">
+        <h3 className="font-medium text-white">
+            John Doe
+        </h3>
+
+        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+        I can fly
+        </p>
+
+    </div>
+    </a>
+    </article>
+
+    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+        <a href='#'>
+    <img
+        alt="Office"
+        src={coverimg}
+        className="w-full h-40 p-4 object-cover"
+    />
+
+    <div className="p-4 sm:p-6">
+        <h3 className="font-medium text-white">
+            John Doe
+        </h3>
+
+        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+        I can fly
+        </p>
+
+    </div>
+    </a>
+    </article>
+
+    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+        <a href='#'>
+    <img
+        alt="Office"
+        src={coverimg}
+        className="w-full h-40 p-4 object-cover"
+    />
+
+    <div className="p-4 sm:p-6">
+        <h3 className="font-medium text-white">
+            John Doe
+        </h3>
+
+        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+        I can fly
+        </p>
+
+    </div>
+    </a>
+    </article>
+
+</div>
+
+
     </div>
   )
 }
