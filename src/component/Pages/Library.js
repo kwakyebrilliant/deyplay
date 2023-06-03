96eef58f53b5c4c183af82ad53c5d3ecd79a1ab0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-03 20:46:37 +0000

    library updates

--- src/component/Pages/Library.js
+++ src/component/Pages/Library.js
@@ -5,6 +5,19 @@ import AlbumCard from '../Cards/AlbumCard'
 import MusicCard from '../Cards/MusicCard'
 
 function Library() {
+
+    var myDate = new Date();
+    var hours= myDate.getHours();
+    var greet;
+  
+    if (hours < 12)
+        greet =  "morning";
+    else if (hours >= 12 && hours <= 17)
+        greet = "afternoon";
+    else if (hours >= 17 && hours <= 24)
+        greet = "evening";
+
+
     return (
         <div>
             <div className='bg-black/80'>
@@ -23,7 +36,7 @@ function Library() {
                         <div className="sm:justify-between sm:items-center sm:flex">
                         <div className="text-center sm:text-left">
                             <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
-                            Welcome Back, Brilliant!
+                            Good {greet}, 0xgt...4bxe
                             </h1>
 
                             <p className="mt-1.5 text-sm text-gray-900">
