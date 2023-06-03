4072196a82f4a6932b6f1cb186635f10009fdaa9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-03 23:53:32 +0000

    albumsingle updates

--- src/component/Singles/AlbumSingle.js
+++ src/component/Singles/AlbumSingle.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Sidebar from '../Partials/Sidebar'
+import PartialNavbar from '../Partials/PartialNavbar'
+
 
 function AlbumSingle() {
   return (
-    <div>AlbumSingle</div>
+    <div>
+        <div className='bg-black/80'>
+        <>
+
+        <div className='flex flex-auto'>
+            <Sidebar />
+
+            <div className='grow'>
+                <PartialNavbar />
+
+                
+
+
+            </div>
+
+
+        </div>
+        
+        </>
+
+        </div>
+    </div>
   )
 }
 
