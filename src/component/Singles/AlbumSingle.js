cdfc4990b67b5ed85b816ec67df5d2abc55ce0c3
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-04 09:38:33 +0000

    albumsingle updates

--- src/component/Singles/AlbumSingle.js
+++ src/component/Singles/AlbumSingle.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import Sidebar from '../Partials/Sidebar'
@@ -19,7 +20,7 @@ function AlbumSingle() {
 
                 <div className='mx-3 mt-8'>
 
-                    <div className='relative grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-16'>
+                    <div className='relative grid grid-cols-1 lg:grid-cols-2'>
 
 
                     <article className="overflow-hidden lg:w-96 mb-8 rounded-lg border border-black/80 bg-black shadow-sm">
@@ -53,7 +54,7 @@ function AlbumSingle() {
                             <p className="text-lg font-bold">1</p>
                         </div>
                         <div className="">
-                            <img src={gm} alt="Image" className="w-60 h-28 rounded-lg object-cover" />
+                            <img src={gm} alt="Image" className="w-52 h-28 rounded-lg object-cover" />
                         </div>
                         <div className="p-4">
                             <p className="text-lg font-bold mb-2">Title</p>
