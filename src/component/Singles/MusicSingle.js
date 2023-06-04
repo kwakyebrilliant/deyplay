77869594c5f32858eb6744b931f148a73a36d91f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-04 00:00:43 +0000

    musicsingle added

---
+++ src/component/Singles/MusicSingle.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Sidebar from '../Partials/Sidebar'
+import PartialNavbar from '../Partials/PartialNavbar'
+
+
+function MusicSingle() {
+  return (
+    <div>
+        <div className='bg-black/80'>
+        <>
+
+        <div className='flex flex-auto'>
+            <Sidebar />
+
+            <div className='grow'>
+                <PartialNavbar />
+
+                
+
+
+            </div>
+
+
+        </div>
+        
+        </>
+
+        </div>
+    </div>
+  )
+}
+
+export default MusicSingle
