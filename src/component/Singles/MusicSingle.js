3a61fb16bda186ce119a21b734fbd2345f892ecb
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-04 12:20:30 +0000

    musicsingle updates

--- src/component/Singles/MusicSingle.js
+++ src/component/Singles/MusicSingle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sidebar from '../Partials/Sidebar'
 import PartialNavbar from '../Partials/PartialNavbar'
+import coverimg from '../assets/coverimg.jpeg';
 
 
 function MusicSingle() {
@@ -15,6 +16,22 @@ function MusicSingle() {
             <div className='grow'>
                 <PartialNavbar />
 
+
+                <div className="relative mx-2">
+                        <img src={coverimg} alt="Background" className="w-full rounded object-cover h-96" />
+
+                        <div className="absolute bg-black/30 inset-0 flex flex-col items-center justify-end p-6">
+                        
+                        <h3 className="text-xl font-medium text-white">
+                            John Doe
+                        </h3>
+                        <p class="mt-1.5 max-w-[40ch] text-xs text-white">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sequi
+                        dicta impedit aperiam ipsum!
+                        </p>
+                        </div>
+                   
+                    </div>
                 
 
 
