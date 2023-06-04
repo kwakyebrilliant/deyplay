56284c8de8c62d337222daf01a0672886b114f21
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-04 09:10:26 +0000

    albumsingle updates

--- src/component/Singles/AlbumSingle.js
+++ src/component/Singles/AlbumSingle.js
@@ -22,7 +22,7 @@ function AlbumSingle() {
                     <div className='relative grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-16'>
 
 
-                    <article className="overflow-hidden w-96 mb-8 rounded-lg border border-black/80 bg-black shadow-sm">
+                    <article className="overflow-hidden lg:w-96 mb-8 rounded-lg border border-black/80 bg-black shadow-sm">
                     <a>
                     <img
                         alt="Office"
