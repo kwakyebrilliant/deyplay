83a51a0b0993d7b937436f9821a5eb96cdce64af
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-05 17:19:03 +0000

    dashlibrary updates

--- src/dashboard/Pages/DashLibrary.js
+++ src/dashboard/Pages/DashLibrary.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Sidebar from '../Partials/Sidebar';
+import PartialNavbar from '../Partials/PartialNavbar';
 
 function DashLibrary() {
   return (
-    <div>DashLibrary</div>
+    <div>
+        <div className='bg-black/80'>
+        <>
+        <div className='flex flex-auto'>
+            <Sidebar />
+
+            <div className='grow'>
+                <PartialNavbar />
+
+                
+
+            </div>
+
+        </div>
+        </>
+        
+        </div>
+    </div>
   )
 }
 
