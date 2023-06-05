37d4be5c37b139997dc280ac46da1a141929a7b1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-05 10:33:01 +0000

    index.css updates

--- src/dashboard/Pages/Dashboard.js
+++ src/dashboard/Pages/Dashboard.js
@@ -151,6 +151,36 @@ function Dashboard() {
                     </div>
 
 
+                    <div className='flex m-3 pt-4'>
+                    <h2 className='text-white font-bold'>
+                        What next?
+                    </h2>
+                
+                    </div>
+
+                    <span
+  class="inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm"
+>
+  <button
+    class="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
+  >
+    Edit
+  </button>
+
+  <button
+    class="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
+  >
+    View
+  </button>
+
+  <button
+    class="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
+  >
+    Delete
+  </button>
+</span>
+
+
 
             </div>
 
