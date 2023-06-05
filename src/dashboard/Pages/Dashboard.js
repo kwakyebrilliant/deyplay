445508ef2cec43858804ae3b03634c88f16692c6
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-05 09:51:14 +0000

    dashboard updates

--- src/dashboard/Pages/Dashboard.js
+++ src/dashboard/Pages/Dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Sidebar from '../Partials/Sidebar';
 import PartialNavbar from '../Partials/PartialNavbar';
+import gm from '../../component/assets/gm.jpeg';
+import kl from '../../component/assets/kl.jpeg';
+import coverimg from '../../component/assets/coverimg.jpeg';
 
 function Dashboard() {
 
@@ -49,10 +52,102 @@ function Dashboard() {
 
                 <div className='flex m-3 pt-4'>
                   <h2 className='text-white font-bold'>
-                    These statistics
+                    These Statistics
                   </h2>
             
                 </div>
+
+
+                <div className='relative m-3 grid grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-16'>
+
+
+                    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+                            <a href='/musicsingle'>
+                        <img
+                            alt="Office"
+                            src={coverimg}
+                            className="w-full h-40 p-4 object-cover"
+                        />
+
+                        <div className="p-4 sm:p-6">
+                            <h3 className="font-medium text-white">
+                                John Doe
+                            </h3>
+
+                            <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+                            I can fly
+                            </p>
+
+                        </div>
+                        </a>
+                        </article>
+
+                        <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+                            <a href='/musicsingle'>
+                        <img
+                            alt="Office"
+                            src={gm}
+                            className="w-full h-40 p-4 object-cover"
+                        />
+
+                        <div className="p-4 sm:p-6">
+                            <h3 className="font-medium text-white">
+                                John Doe
+                            </h3>
+
+                            <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+                            I can fly
+                            </p>
+
+                        </div>
+                        </a>
+                        </article>
+
+                        <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+                            <a href='/musicsingle'>
+                        <img
+                            alt="Office"
+                            src={kl}
+                            className="w-full h-40 p-4 object-cover"
+                        />
+
+                        <div className="p-4 sm:p-6">
+                            <h3 className="font-medium text-white">
+                                John Doe
+                            </h3>
+
+                            <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+                            I can fly
+                            </p>
+
+                        </div>
+                        </a>
+                        </article>
+
+                        <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+                            <a href='/musicsingle'>
+                        <img
+                            alt="Office"
+                            src={gm}
+                            className="w-full h-40 p-4 object-cover"
+                        />
+
+                        <div className="p-4 sm:p-6">
+                            <h3 className="font-medium text-white">
+                                John Doe
+                            </h3>
+
+                            <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+                            I can fly
+                            </p>
+
+                        </div>
+                        </a>
+                        </article>
+
+                       
+
+                    </div>
 
 
 
