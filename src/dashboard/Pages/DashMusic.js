2be97b106e32d22392256e9a4d702a16d2a5c388
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-05 17:27:35 +0000

    dashmusics updates

--- src/dashboard/Pages/DashMusic.js
+++ src/dashboard/Pages/DashMusic.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Sidebar from '../Partials/Sidebar';
 import PartialNavbar from '../Partials/PartialNavbar';
-import coverimg from '../../component/assets/coverimg.jpeg';
+import musics from '../../component/assets/musics.jpg';
 
 function DashMusic() {
   return (
@@ -16,7 +16,7 @@ function DashMusic() {
 
 
                 <div className="relative mx-2">
-                        <img src={coverimg} alt="Background" className="w-full rounded object-cover h-96" />
+                        <img src={musics} alt="Background" className="w-full rounded object-cover h-96" />
 
                         <div className="absolute bg-black/30 inset-0 flex flex-col items-center justify-end p-6">
                         
