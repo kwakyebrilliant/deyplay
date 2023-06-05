b4e0ec63019a09c7f078475054c9630ce6317da2
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-05 10:39:57 +0000

    dashboard updates

--- src/dashboard/Pages/Dashboard.js
+++ src/dashboard/Pages/Dashboard.js
@@ -151,34 +151,23 @@ function Dashboard() {
                     </div>
 
 
-                    <div className='flex m-3 pt-4'>
+                    <div className='flex mt-12 justify-center m-3 pt-4'>
                     <h2 className='text-white font-bold'>
                         What next?
                     </h2>
                 
                     </div>
 
-                    <span
-  class="inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm"
->
-  <button
-    class="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
-  >
-    Edit
-  </button>
-
-  <button
-    class="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
-  >
-    View
-  </button>
-
-  <button
-    class="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
-  >
-    Delete
-  </button>
-</span>
+                    <div className="flex mb-12 justify-center items-center">
+                    <span className="inline-flex justify-center -space-x-px overflow-hidden rounded-md border bg-white shadow-sm">
+                        <button className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative">
+                        Add Albums
+                        </button>
+                        <button className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative">
+                        Add Musics
+                        </button>
+                    </span>
+                    </div>
 
 
 
