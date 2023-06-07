c247d843264f3ca53b442a407af5db8fb7871f0b
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-07 09:12:34 +0000

    dashlibrary updates

--- src/dashboard/Pages/DashLibrary.js
+++ src/dashboard/Pages/DashLibrary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sidebar from '../Partials/Sidebar';
 import PartialNavbar from '../Partials/PartialNavbar';
+import library from '../../component/assets/library.jpg';
 
 function DashLibrary() {
   return (
@@ -12,6 +13,23 @@ function DashLibrary() {
 
             <div className='grow'>
                 <PartialNavbar />
+
+
+                <div className="relative mx-2">
+                        <img src={library} alt="Background" className="w-full rounded object-cover h-96" />
+
+                        <div className="absolute bg-black/30 inset-0 flex flex-col items-center justify-end p-6">
+                        
+                        <h3 className="text-xl font-medium text-white">
+                          0xgt...4bxe
+                        </h3>
+                        <p class="mt-1.5 max-w-[40ch] text-xs text-white">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sequi
+                        dicta impedit aperiam ipsum!
+                        </p>
+                        </div>
+                   
+                </div>
 
                 
 
