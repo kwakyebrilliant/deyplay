65a2a6058d1900e9de15eed3230c95ca843a42e8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-07 09:16:32 +0000

    dashlibrary updates

--- src/dashboard/Pages/DashLibrary.js
+++ src/dashboard/Pages/DashLibrary.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Sidebar from '../Partials/Sidebar';
 import PartialNavbar from '../Partials/PartialNavbar';
 import library from '../../component/assets/library.jpg';
+import { FiSearch } from 'react-icons/fi';
 
 function DashLibrary() {
   return (
@@ -19,9 +20,20 @@ function DashLibrary() {
                         <img src={library} alt="Background" className="w-full rounded object-cover h-96" />
 
                         <div className="absolute bg-black/30 inset-0 flex flex-col items-center justify-end p-6">
+
+                        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-pink-500 p-2">
+                            <input
+                            className="w-64 bg-transparent focus:outline-none text-white placeholder-white"
+                            type="text"
+                            placeholder="Search..."
+                            />
+                            <button className="ml-2 text-white">
+                            <FiSearch className='text-white' />
+                            </button>
+                        </div>
                         
                         <h3 className="text-xl font-medium text-white">
-                          0xgt...4bxe
+                            John Doe
                         </h3>
                         <p class="mt-1.5 max-w-[40ch] text-xs text-white">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sequi
@@ -29,7 +41,7 @@ function DashLibrary() {
                         </p>
                         </div>
                    
-                </div>
+                    </div>
 
 
                 <div className='flex m-3 pt-4'>
