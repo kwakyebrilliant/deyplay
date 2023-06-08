518909d29242130db37c30ceb01900063b4a33d7
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-08 14:10:37 +0000

    deyplay updates

--- contracts/Deyplay.sol
+++ contracts/Deyplay.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.4;
 
 contract Deyplay{
+
+    struct Album {
+        uint id;
+        string title;
+        address artist;
+        string imageUrl;
+        string description;
+        uint price;
+        uint[] audioFiles;
+        mapping(address => uint) royalties;
+        uint totalStreams;
+        uint totalPurchases;
+    }
     
 }
