46bad1025297f805c0cf938f62603c501fe10a01
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-08 15:12:25 +0000

    deyplay updates

--- contracts/Deyplay.sol
+++ contracts/Deyplay.sol
@@ -13,8 +13,10 @@ contract Deyplay{
         uint totalStreams;
         uint totalPurchases;
         uint[] audioFiles;
-        mapping(address => uint) royalties;
+        address[] royaltiesOwners;
+        uint[] royaltiesPercentages;
     }
+
 
     //Album struct
     struct Album {
@@ -27,7 +29,8 @@ contract Deyplay{
         uint totalStreams;
         uint totalPurchases;
         uint[] audioFiles;
-        mapping(address => uint) royalties;
+        address[] royaltiesOwners;
+        uint[] royaltiesPercentages;
     }
 
 
@@ -36,34 +39,40 @@ contract Deyplay{
     uint private albumCount;
     mapping(uint => Track) private tracks;
     mapping(uint => Album) private albums;
-
     mapping(address => uint[]) private userPurchasedTracks;
     mapping(address => uint[]) private userPurchasedAlbums;
-
     mapping(address => uint) private artistBalances;
 
 
     //Events
-    event TrackAdded(uint trackId, string title, address artist, string imageUrl, uint price);
-    event AlbumAdded(uint albumId, string title, address artist, string imageUrl, uint price);
-    event TrackStreamed(uint trackId, address listener, uint amount);
-    event AlbumStreamed(uint albumId, address listener, uint amount);
-    event TrackPurchased(uint trackId, address buyer, uint amount);
-    event AlbumPurchased(uint albumId, address buyer, uint amount);
+    event TrackCreated(uint id, string title, address artist, string imageUrl, uint price);
+    event AlbumCreated(uint id, string title, string description, address artist, string imageUrl, uint price);
+    event TrackStreamed(uint trackId, address user, uint amount);
+    event AlbumStreamed(uint albumId, address user, uint amount);
+    event TrackPurchased(uint trackId, address user, uint amount);
+    event AlbumPurchased(uint albumId, address user, uint amount);
 
-    //Modifiers
+
+    //Modiefiers
     modifier trackExists(uint _trackId) {
-        require(_trackId <= trackCount, "Track does not exist");
+        require(_trackId > 0 && _trackId <= trackCount, "Track does not exist");
         _;
     }
 
     modifier albumExists(uint _albumId) {
-        require(_albumId <= albumCount, "Album does not exist");
+        require(_albumId > 0 && _albumId <= albumCount, "Album does not exist");
         _;
     }
 
 
-    
+    //Add track function
+    function addTrack(string memory _title, string memory _imageUrl, uint _price, address[] memory _royaltiesOwners, uint[] memory _royaltiesPercentages) public {
+            trackCount++;
+            uint[] memory audioFiles;
+            tracks[trackCount] = Track(trackCount, _title, msg.sender, _imageUrl, _price, 0, 0, audioFiles, _royaltiesOwners, _royaltiesPercentages);
+
+            emit TrackCreated(trackCount, _title, msg.sender, _imageUrl, _price);
+    }
 
     
 }
