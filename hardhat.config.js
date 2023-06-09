359acb9bf2bf2538e09f9fe86c447fe6e7c933c4
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-09 11:39:54 +0000

    updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
+const privateKey = '';
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1313161555,
+    },
+    testnet_aurora: {
+      url: 'https://testnet.aurora.dev',
+      accounts: [privateKey],
+    },
+  },
+  solidity: {
+    version: '0.8.4',
+      paths: {
+    artifacts: './src/artifacts',
+  },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+    },
+  },
 };
