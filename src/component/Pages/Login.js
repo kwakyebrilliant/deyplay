ee98a0599988e5ffe1f220bc63110586297977ba
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-10 12:32:18 +0000

    login updates

--- src/component/Pages/Login.js
+++ src/component/Pages/Login.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from "react-router-dom";
 
 function Login() {
+    const [user, setUser] = useState('');
+
   return (
     <div>
 
 <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
   <div className="mx-auto max-w-lg">
-    <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
+    <h1 className="text-center text-2xl font-bold text-black sm:text-3xl">
       Connect Address
     </h1>
 
@@ -16,46 +19,47 @@ function Login() {
     </p>
 
     <form
-      action=""
       className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
     >
       <p className="text-center text-lg font-medium">Choose user type</p>
 
       <div>
-        <label for="email" className="sr-only">Email</label>
-
-        <div className="relative">
-          <input
-            type="email"
-            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            placeholder="Enter email"
-          />
-
-          <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-              />
-            </svg>
-          </span>
-        </div>
+      <select
+        className="w-full rounded-lg border-gray-200 p-4 pe-12 mb-4 text-sm shadow-sm"
+        required onChange={(e) => { setUser(e.target.value) }}
+        >
+            <option>Please select</option>
+            <option value="user">User</option>
+            <option value="artiste">Artiste</option>
+        </select>
+       
       </div>
 
-      <button
-        type="submit"
-        className="block w-full rounded-lg bg-black px-5 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-black border-black"
-      >
-        Connect Wallet
-      </button>
+           {
+                user ==='user' && (
+                    <Link to="/">
+                    <button
+                    type="submit"
+                    className="block w-full rounded-lg bg-black px-5 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-black border-black"
+                  >
+                    Connect Wallet
+                  </button>
+                  </Link>
+                )
+            } 
+            {
+                user ==='artiste' && (
+                    <Link to="/dashboard">
+                    <button
+                    type="submit"
+                    className="block w-full rounded-lg bg-black px-5 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-black border-black"
+                  >
+                    Connect Wallet
+                  </button>
+                  </Link>
+                )
+            } 
+
     </form>
   </div>
 </div>
