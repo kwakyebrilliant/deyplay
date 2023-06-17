57e08c939fe8f07a69099b9611dcc148b4061780
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-17 21:10:58 +0000

    dashlibrary updates

--- src/dashboard/Pages/DashLibrary.js
+++ src/dashboard/Pages/DashLibrary.js
@@ -3,9 +3,6 @@ import React, { useState, useEffect } from 'react';
 import Sidebar from '../Partials/Sidebar';
 import PartialNavbar from '../Partials/PartialNavbar';
 import library from '../../component/assets/library.jpg';
-import coverimg from '../../component/assets/coverimg.jpeg';
-import gm from '../../component/assets/gm.jpeg';
-import kl from '../../component/assets/kl.jpeg';
 import { FiSearch } from 'react-icons/fi';
 
 import { ethers } from 'ethers';
@@ -20,24 +17,26 @@ function DashLibrary() {
     const [currentAccount, setCurrentAccount] = useState('');
   
     useEffect(() => {
-      connectToMetaMask();
-    }, []);
-  
-    async function connectToMetaMask() {
-      try {
-        await window.ethereum.enable();
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        const contract = new ethers.Contract(deyplayAddress, Deyplay.abi, signer);
-  
-        const accounts = await provider.listAccounts();
-        setCurrentAccount(accounts[0]);
-  
-        fetchTracksByArtist(contract, accounts[0]);
-      } catch (error) {
-        console.error('Error connecting to MetaMask:', error);
+        connectToMetaMask();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);
+    
+      async function connectToMetaMask() {
+        try {
+          await window.ethereum.enable();
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(deyplayAddress, Deyplay.abi, signer);
+    
+          const accounts = await provider.listAccounts();
+          setCurrentAccount(accounts[0]);
+    
+          fetchTracksByArtist(contract, accounts[0]);
+          fetchAlbumsByArtist(contract, accounts[0]);
+        } catch (error) {
+          console.error('Error connecting to MetaMask:', error);
+        }
       }
-    }
   
     async function fetchTracksByArtist(contract, artistAddress) {
       try {
@@ -63,6 +62,29 @@ function DashLibrary() {
       }
     }
     
+    async function fetchAlbumsByArtist(contract, artistAddress) {
+        try {
+          const albumIds = await contract.listAlbumsByArtist(artistAddress);
+    
+          const albumDetails = await Promise.all(
+            albumIds.map(async (albumId) => {
+              const album = await contract.getAlbum(albumId);
+              return {
+                id: album.id,
+                title: album.title,
+                artist: album.artist,
+                imageUrl: album.imageUrl,
+                description: album.description,
+                price: album.price,
+              };
+            })
+          );
+    
+          setAlbums(albumDetails);
+        } catch (error) {
+          console.error('Error fetching albums by artist:', error);
+        }
+      }
 
 
   return (
@@ -112,144 +134,44 @@ function DashLibrary() {
                 
                 </div>
 
-                <div className='mx-3'>
+                <div className='mx-3 mb-12'>
+                {albums.length === 0 ? (
+                        <h1 className="font-bold text-5xl text-center text-white">No albums available</h1>
+                    ) : (
+                  <div className='relative grid grid-cols-2 lg:grid-cols-6 gap-x-8 gap-y-16'>
 
-                <div className='relative grid grid-cols-2 lg:grid-cols-6 gap-x-8 gap-y-16'>
+                {albums.map((album) => (
 
-
-                <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
-                        <a>
-                    <img
-                        alt="Office"
-                        src={gm}
-                        className="w-full h-40 p-4 object-cover"
-                    />
-
-
-                    <div className="p-4 sm:p-6">
-                        <h3 className="font-medium text-white">
-                            John Doe
-                        </h3>
-
-                        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
-                        I can fly
-                        </p>
-
-                    </div>
+                  <article key={album.id} className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
+                          <a href='dashmusicsingle'>
+                      <img
+                          src={album.imageUrl} 
+                          alt={album.title} 
+                          className="w-full h-40 p-4 object-cover"
+                      />
 
 
-                    </a>
-                    </article>
+                      <div className="p-4 sm:p-6">
+                          <h3 className="font-medium text-white">
+                          {album.artist.slice(0, 6)}…{album.artist.slice(album.artist.length - 6)}
+                          </h3>
 
-                    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
-                        <a>
-                    <img
-                        alt="Office"
-                        src={coverimg}
-                        className="w-full h-40 p-4 object-cover"
-                    />
+                          <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+                          {album.title}
+                          </p>
 
-                    <div className="p-4 sm:p-6">
-                        <h3 className="font-medium text-white">
-                            John Doe
-                        </h3>
-
-                        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
-                        I can fly
-                        </p>
-
-                    </div>
-                    </a>
-                    </article>
-
-                    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
-                        <a>
-                    <img
-                        alt="Office"
-                        src={gm}
-                        className="w-full h-40 p-4 object-cover"
-                    />
-
-                    <div className="p-4 sm:p-6">
-                        <h3 className="font-medium text-white">
-                            John Doe
-                        </h3>
-
-                        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
-                        I can fly
-                        </p>
-
-                    </div>
-                    </a>
-                    </article>
-
-                    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
-                        <a>
-                    <img
-                        alt="Office"
-                        src={kl}
-                        className="w-full h-40 p-4 object-cover"
-                    />
-
-                    <div className="p-4 sm:p-6">
-                        <h3 className="font-medium text-white">
-                            John Doe
-                        </h3>
-
-                        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
-                        I can fly
-                        </p>
-
-                    </div>
-                    </a>
-                    </article>
-
-                    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
-                        <a>
-                    <img
-                        alt="Office"
-                        src={coverimg}
-                        className="w-full h-40 p-4 object-cover"
-                    />
-
-                    <div className="p-4 sm:p-6">
-                        <h3 className="font-medium text-white">
-                            John Doe
-                        </h3>
-
-                        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
-                        I can fly
-                        </p>
-
-                    </div>
-                    </a>
-                    </article>
-
-                    <article className="overflow-hidden rounded-lg border border-black/80 bg-black shadow-sm">
-                        <a>
-                    <img
-                        alt="Office"
-                        src={coverimg}
-                        className="w-full h-40 p-4 object-cover"
-                    />
-
-                    <div className="p-4 sm:p-6">
-                        <h3 className="font-medium text-white">
-                            John Doe
-                        </h3>
-
-                        <p className="line-clamp-3 text-sm/relaxed text-gray-500">
-                        I can fly
-                        </p>
-
-                    </div>
-                    </a>
-                    </article>
-
-                </div>
+                      </div>
 
 
-                </div>
+                      </a>
+                      </article>
+
+                  ))}
+                  </div>
+                  )}
+
+
+                  </div>
 
 
 
