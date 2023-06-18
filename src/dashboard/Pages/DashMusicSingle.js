199ae9237a830c698e5a14851bf2d0a91ea6351d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-18 11:39:13 +0000

    updates

--- src/dashboard/Pages/DashMusicSingle.js
+++ src/dashboard/Pages/DashMusicSingle.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
 import Sidebar from '../Partials/Sidebar';
 import PartialNavbar from '../Partials/PartialNavbar';
 import { FaUpload, FaMoneyBill, FaEye } from 'react-icons/fa';
@@ -13,115 +13,11 @@ const deyplayAddress = "0xf3d257D4Ee9AF5B663e1D3543CBe59eC98459B39";
 function DashMusicSingle() {
 
     let location = useLocation();
-    const trackss = location.state;
+    const tracks = location.state;
     console.log(location);
-    
-
-    const [tracks, setTracks] = useState([]);
-    const [albums, setAlbums] = useState([]);
-    const [currentAccount, setCurrentAccount] = useState('');
-    const [selectedAlbumId, setSelectedAlbumId] = useState('');
-    const [selectedTrackId, setSelectedTrackId] = useState('');
-
-    useEffect(() => {
-        connectToMetaMask();
-      }, []);
-    
-      async function connectToMetaMask() {
-        try {
-          await window.ethereum.enable();
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner();
-          const contract = new ethers.Contract(deyplayAddress, Deyplay.abi, signer);
-    
-          const accounts = await provider.listAccounts();
-          setCurrentAccount(accounts[0]);
-    
-          fetchTracks();
-          fetchAlbums();
-        } catch (error) {
-          console.error('Error connecting to MetaMask:', error);
-        }
-      }
 
 
-      async function fetchTracks() {
-        try {
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const contract = new ethers.Contract(deyplayAddress, Deyplay.abi, provider);
     
-          const trackCount = await contract.getTrackCount();
-          const trackPromises = [];
-          for (let i = 0; i < trackCount; i++) {
-            trackPromises.push(contract.getTrack(i));
-          }
-          const trackResults = await Promise.all(trackPromises);
-          const tracks = trackResults.map((result) => ({
-            id: result[0].toNumber(),
-            title: result[1],
-            artist: result[2],
-            audioFile: result[3],
-            royaltiesOwners: result[4],
-            royaltiesPercentages: result[5],
-            streamCount: result[6].toNumber(),
-            purchaseCount: result[7].toNumber(),
-            price: ethers.utils.formatEther(result[8]),
-            imageUrl: result[9],
-          }));
-    
-          setTracks(tracks);
-        } catch (error) {
-          console.error('Error fetching tracks:', error);
-        }
-      }
-
-      async function fetchAlbums() {
-        try {
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const contract = new ethers.Contract(deyplayAddress, Deyplay.abi, provider);
-    
-          const albumCount = await contract.getAlbumCount();
-          const albumPromises = [];
-          for (let i = 0; i < albumCount; i++) {
-            albumPromises.push(contract.getAlbum(i));
-          }
-          const albumResults = await Promise.all(albumPromises);
-          const albums = albumResults.map((result) => ({
-            id: result[0].toNumber(),
-            title: result[1],
-            artist: result[2],
-            description: result[3],
-            royaltiesOwners: result[4],
-            royaltiesPercentages: result[5],
-            trackIds: result[6].map((trackId) => trackId.toNumber()),
-            streamCount: result[7].toNumber(),
-            purchaseCount: result[8].toNumber(),
-            price: ethers.utils.formatEther(result[9]),
-            imageUrl: result[10],
-          }));
-    
-          setAlbums(albums);
-        } catch (error) {
-          console.error('Error fetching albums:', error);
-        }
-      }
-
-
-      async function addTrackToAlbum() {
-        try {
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner();
-          const contract = new ethers.Contract(deyplayAddress, Deyplay.abi, signer);
-    
-          await contract.addTrackToAlbum(selectedTrackId, selectedAlbumId);
-    
-          // Refresh the tracks and albums after adding the track to the album
-          fetchTracks();
-          fetchAlbums();
-        } catch (error) {
-          console.error('Error adding track to album:', error);
-        }
-      }
 
   return (
     <div>
@@ -135,23 +31,23 @@ function DashMusicSingle() {
                     <PartialNavbar />
 
                     <div className="relative mx-2">
-                        <img src={trackss.imageUrl} alt={trackss.title}  className="w-full rounded object-cover h-96" />
+                        <img src={tracks.imageUrl} alt={tracks.title}  className="w-full rounded object-cover h-96" />
 
                         <div className="absolute bg-black/30 inset-0 flex flex-col items-center justify-end p-6">
                         
                         <h3 className="text-xl font-medium text-white">
-                        {trackss.artist.slice(0, 6)}…{trackss.artist.slice(trackss.artist.length - 6)}
+                        {tracks.artist.slice(0, 6)}…{tracks.artist.slice(tracks.artist.length - 6)}
                         </h3>
                         <p className="line-clamp-3 text-sm/relaxed text-white">
-                          {trackss.title}
+                          {tracks.title}
                           </p>
                         <div className="px-4 pt-4 sm:px-6">
                             <div className='flex'>
                                 <h3 className="font-bold flex text-sm/relaxed text-white">
-                                <FaEye className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(trackss.totalStreams)}
+                                <FaEye className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(tracks.totalStreams)}
                                 </h3>
                                 <h3 className="font-bold flex px-2 text-sm/relaxed text-white">
-                                <FaMoneyBill className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(trackss.totalPurchases)}
+                                <FaMoneyBill className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(tracks.totalPurchases)}
                                 </h3>
                             </div>
                         </div>
@@ -159,7 +55,7 @@ function DashMusicSingle() {
                         <div className="mt-4 flex flex-col items-center">
 
                         <h3 className="font-bold text-base bg-white p-2 rounded-e-full text-black">
-                            ${ethers.utils.formatEther(trackss.price)}
+                            ${ethers.utils.formatEther(tracks.price)}
                         </h3>
                         
                         </div>
