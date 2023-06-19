9b22aa9b7fc89af5ca4a93c5c6710f2b2ac16ada
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-19 10:15:14 +0000

    dashmusicsingle updates

--- src/dashboard/Pages/DashMusicSingle.js
+++ src/dashboard/Pages/DashMusicSingle.js
@@ -13,7 +13,7 @@ import { ethers } from 'ethers';
 function DashMusicSingle() {
 
     let location = useLocation();
-    const trackss = location.state;
+    const tracks = location.state;
     console.log(location);
 
     
@@ -30,23 +30,21 @@ function DashMusicSingle() {
                     <PartialNavbar />
 
                     <div className="relative mx-2">
-                        <img src={trackss.imageUrl} alt={trackss.title}  className="w-full rounded object-cover h-96" />
+                        <img src={tracks.imageUrl} alt={tracks.title}  className="w-full rounded object-cover h-96" />
 
                         <div className="absolute bg-black/30 inset-0 flex flex-col items-center justify-end p-6">
                         
                         <h3 className="text-xl font-medium text-white">
-                        {trackss.artist.slice(0, 6)}…{trackss.artist.slice(trackss.artist.length - 6)}
+                        {tracks.artist.slice(0, 6)}…{tracks.artist.slice(tracks.artist.length - 6)}
                         </h3>
-                        <p className="line-clamp-3 text-sm/relaxed text-white">
-                          {trackss.title}
-                          </p>
+
                         <div className="px-4 pt-4 sm:px-6">
                             <div className='flex'>
                                 <h3 className="font-bold flex text-sm/relaxed text-white">
-                                <FaEye className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(trackss.totalStreams)}
+                                <FaEye className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(tracks.totalStreams)}
                                 </h3>
                                 <h3 className="font-bold flex px-2 text-sm/relaxed text-white">
-                                <FaMoneyBill className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(trackss.totalPurchases)}
+                                <FaMoneyBill className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(tracks.totalPurchases)}
                                 </h3>
                             </div>
                         </div>
@@ -54,7 +52,7 @@ function DashMusicSingle() {
                         <div className="mt-4 flex flex-col items-center">
 
                         <h3 className="font-bold text-base bg-white p-2 rounded-e-full text-black">
-                            ${ethers.utils.formatEther(trackss.price)}
+                            ${ethers.utils.formatEther(tracks.price)}
                         </h3>
                         
                         </div>
@@ -69,10 +67,15 @@ function DashMusicSingle() {
                     </div>
 
 
-                    <div className='flex m-3 mb-40 pt-4'>
+                    <div className='flex mx-3 mb-40 pt-4'>
                         <div className="flex w-full">
                             <div className="px-8 w-full">
-                                
+                                  <p className="line-clamp-3 text-sm/relaxed text-white">
+                                    {tracks.title}
+                                  </p>
+                                  <audio controls className="my-4 w-full">
+                                    <source src={tracks.audioFile} />
+                                  </audio>
 
                             </div>
                         </div>
