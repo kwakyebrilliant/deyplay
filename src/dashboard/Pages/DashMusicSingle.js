ddf5b5d5cabcbe8ecc2879236d07af1295b078c1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-20 22:59:21 +0000

    dashmusicsingle updates

--- src/dashboard/Pages/DashMusicSingle.js
+++ src/dashboard/Pages/DashMusicSingle.js
@@ -76,6 +76,12 @@ function DashMusicSingle() {
 
                   <div className="px-4 py-4 sm:px-6">
                     <h3 className="text-sm/relaxed text-white">
+                    Stream Amount- ${ethers.utils.formatEther(tracks.streamAmount)}
+                    </h3>
+                  </div>
+
+                  <div className="px-4 py-4 sm:px-6">
+                    <h3 className="text-sm/relaxed text-white">
                       {tracks.description}
                     </h3>
                   </div>
