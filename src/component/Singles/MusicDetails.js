fef3b89c637614f955a0c22dd7b02020b659ec1a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-20 10:03:18 +0000

    music details updates

--- src/component/Singles/MusicDetails.js
+++ src/component/Singles/MusicDetails.js
@@ -1,14 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Sidebar from '../Partials/Sidebar'
 import PartialNavbar from '../Partials/PartialNavbar'
 
+import { useLocation } from 'react-router-dom';
+
 import { ethers } from 'ethers';
-import Deyplay from '../../artifacts/contracts/Deyplay.sol/Deyplay.json';
-const deyplayAddress = "0xE94C4eF5f0ac10D6dfa9338FcAFB6fF67841FAB0";
 
 function MusicDetails() {
+    let location = useLocation();
+    const tracks = location.state;
+
+    const audioRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [progress, setProgress] = useState(0);
+
+    const handleTogglePlay = () => {
+        if (isPlaying) {
+        audioRef.current.pause();
+        } else {
+        audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    };
+
+    const handleProgressChange = () => {
+        const progressValue = (audioRef.current.currentTime / audioRef.current.duration) * 100;
+        setProgress(progressValue);
+    };
+
   return (
-    <div>MusicDetails</div>
+    <div>
+         <div className='bg-black/80'>
+            <>
+
+            <div className='flex flex-auto'>
+                <Sidebar />
+
+                <div className='grow'>
+
+                    <PartialNavbar />
+
+                    <div className='mx-3 mt-8 mb-12'>
+                        
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
+            
+            </>
+        </div>
+
+    </div>
   )
 }
 
