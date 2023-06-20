1be7c95a26f635dcd29c1508108c55bec162406d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-20 09:56:42 +0000

    music details updates

--- src/component/Singles/MusicDetails.js
+++ src/component/Singles/MusicDetails.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Sidebar from '../Partials/Sidebar'
+import PartialNavbar from '../Partials/PartialNavbar'
+
+import { ethers } from 'ethers';
+import Deyplay from '../../artifacts/contracts/Deyplay.sol/Deyplay.json';
+const deyplayAddress = "0xE94C4eF5f0ac10D6dfa9338FcAFB6fF67841FAB0";
 
 function MusicDetails() {
   return (
