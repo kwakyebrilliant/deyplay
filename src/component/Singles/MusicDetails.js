a011597d7a9f6ddd1bab561cc84f65435ed0587b
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-20 10:05:16 +0000

    music details updates

--- src/component/Singles/MusicDetails.js
+++ src/component/Singles/MusicDetails.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react';
 import Sidebar from '../Partials/Sidebar'
-import PartialNavbar from '../Partials/PartialNavbar'
+import PartialNavbar from '../Partials/PartialNavbar';
+import { FaMoneyBill, FaEye } from 'react-icons/fa';
+import { PlayIcon, PauseIcon } from '@heroicons/react/solid';
 
 import { useLocation } from 'react-router-dom';
 
@@ -41,6 +43,78 @@ function MusicDetails() {
                     <PartialNavbar />
 
                     <div className='mx-3 mt-8 mb-12'>
+                    <div className='relative grid grid-cols-1'>
+                <article className="overflow-hidden lg:h-min lg:w-full rounded-lg bg-gradient-to-b from-black to-transparent shadow-sm">
+                  <img
+                    alt={tracks.title}
+                    src={tracks.imageUrl}
+                    className="w-full h-96 p-4 object-cover"
+                  />
+
+                  <div className="px-4 pt-4 sm:pt-4 sm:px-6">
+                    <div className='flex justify-between'>
+                      <h3 className="font-bold text-3xl text-white">
+                        {tracks.title}
+                      </h3>
+                      <h3 className="font-bold text-base bg-white p-2 rounded-e-full text-black">
+                        ${ethers.utils.formatEther(tracks.price)}
+                      </h3>
+                    </div>
+
+                    <p className="line-clamp-3 text-sm/relaxed text-gray-500">
+                      {tracks.artist.slice(0, 6)}…{tracks.artist.slice(tracks.artist.length - 6)}
+                    </p>
+                  </div>
+
+                  <div className="px-4 pt-4 sm:px-6">
+                    <div className='flex'>
+                      <h3 className="font-bold flex text-sm/relaxed text-gray-500">
+                        <FaEye className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(tracks.totalStreams)}
+                      </h3>
+                      <h3 className="font-bold flex px-2 text-sm/relaxed text-gray-500">
+                        <FaMoneyBill className=' text-white w-6 h-6 lg:w-6 lg:h-6 pr-1' /> {ethers.utils.formatEther(tracks.totalPurchases)}
+                      </h3>
+                    </div>
+                  </div>
+
+                  <div className="px-4 py-4 sm:px-6">
+                    <h3 className="text-sm/relaxed text-white">
+                      {tracks.description}
+                    </h3>
+                  </div>
+
+                  <div className="px-4 py-4 sm:px-6">
+                  <div className="flex items-center justify-center">
+                    <div className="w-12 h-12 text-black rounded-full bg-white flex items-center justify-center">
+                        {isPlaying ? (
+                        <PauseIcon className="h-6 w-6" onClick={handleTogglePlay} />
+                        ) : (
+                        <PlayIcon className="h-6 w-6" onClick={handleTogglePlay} />
+                        )}
+                    </div>
+                    </div>
+                    <div className="flex items-center mt-4">
+                    <div className="flex-1">
+                        <div className="relative">
+                        <div className="h-1 bg-gray-700 rounded-full">
+                            <div
+                            className="h-1 bg-blue-500 rounded-full"
+                            style={{ width: `${progress}%` }}
+                            ></div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <audio
+                    ref={audioRef}
+                    src={tracks.audioFile}
+                    onTimeUpdate={handleProgressChange}
+                    crossOrigin="anonymous"
+                    />
+                </div>
+
+                </article>
+              </div>
                         
 
                     </div>
